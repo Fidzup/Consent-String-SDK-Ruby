8c58f962a36a8292789467377df4c7361f8c87a7
Nicolas Blanc <sinfomicien@gmail.com>
2018-10-22 18:38:25 +0200

    Correct requirement for the whole library

--- lib/iab_consent_string.rb
+++ lib/iab_consent_string.rb
@@ -1,7 +1,11 @@
-require 'iab_consent_string/vendor_consent'
+require 'iab_consent_string/consent/vendor_consent'
+require 'iab_consent_string/consent/vendor_consent_encoder'
 require 'iab_consent_string/consent/vendor_consent_decoder'
-require 'iab_consent_string/purpose'
+require 'iab_consent_string/consent/implementation/v1/byte_buffer_backed_vendor_consent'
+require 'iab_consent_string/consent/implementation/v1/vendor_consent_builder'
 require 'iab_consent_string/gdpr_constants'
+require 'iab_consent_string/purpose'
+require 'iab_consent_string/util/utils'
 
 module IABConsentString
 end
